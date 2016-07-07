f47778052ee322da953e13bfb1688e23c7bebe91
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-07-07 15:22:01 -0300

    Retirando comentários desnecessários

--- huffman.h
+++ huffman.h
@@ -1,11 +1,8 @@
-//#include "nodo.h"
 #include "lista_encadeada.h"
 
 void abrir_arquivo(FILE **arquivo, char *arquivo_nome);
 
 void contar_caracteres(FILE *arquivo, lista **lista);
-
-//void conta_caracteres(FILE *arquivo, nodo *lista);
 
 void executar(char *arquivo_nome);
 
